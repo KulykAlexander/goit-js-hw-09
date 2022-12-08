e821300fb0d7d7b5e61214cd7f766e8503ed0843
KulykAlexander <44663040+KulykAlexander@users.noreply.github.com>
2022-12-08 20:59:02 +0000

    Deploying to gh-pages from @ KulykAlexander/goit-js-hw-09@5030baf3589de5b6e9886eefc2405e9110b04b84 🚀

---
+++ 01-color-switcher.f63e2dd7.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){changeColor=setInterval((()=>{let e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3),e.disabled=!0})),o.addEventListener("click",(function(){clearInterval(changeColor),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.f63e2dd7.js.map
