5030baf3589de5b6e9886eefc2405e9110b04b84
KulykAlexander <kulikau@gmail.com>
2022-12-08 22:58:38 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const screen = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function startChangeColor() {
+  changeColor = setInterval(() => {
+    let randomColor = getRandomHexColor();
+    screen.style.backgroundColor = randomColor;
+  }, 1000);
+  startBtn.disabled = true;
+}
+function stopChangeColor() {
+  clearInterval(changeColor);
+  startBtn.disabled = false;
+}
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
